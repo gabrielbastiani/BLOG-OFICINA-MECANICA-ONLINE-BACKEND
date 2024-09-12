e028fa34b2b1a487d1c439b3479ad43b33424f3d
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2024-09-12 14:00:39 -0300

    blog

--- src/services/form_contact/FormContactFindService.ts
+++ src/services/form_contact/FormContactFindService.ts
@@ -8,14 +8,18 @@ class FormContactFindService {
     async execute({ form_contact_id }: FormRequest) {
 
         if (form_contact_id) {
-            const forms_all = await prismaClient.form_contact.findMany({
+            const form = await prismaClient.form_contact.findMany({
                 where: {
                     id: form_contact_id
                 }
             });
 
-            return forms_all;
+            return form;
         }
+
+        const form_all = await prismaClient.form_contact.findMany();
+
+        return form_all;
 
     }
 }
