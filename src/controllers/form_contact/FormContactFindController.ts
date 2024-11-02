5642e87d5972284bda363c1f16698e5f7fbf30c5
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2024-11-02 20:21:25 -0300

    blog

--- src/controllers/form_contact/FormContactFindController.ts
+++ src/controllers/form_contact/FormContactFindController.ts
@@ -4,7 +4,15 @@ import { Prisma } from "@prisma/client";
 
 class FormContactFindController {
     async handle(req: Request, res: Response) {
-        const { page = 1, limit = 5, search = "", orderBy = "created_at", orderDirection = "desc" } = req.query;
+        const { 
+            page = 1, 
+            limit = 5, 
+            search = "", 
+            orderBy = "created_at", 
+            orderDirection = "desc",
+            startDate,
+            endDate
+        } = req.query;
 
         const allContacts = new FormContactFindService();
         const contacts = await allContacts.execute(
@@ -12,12 +20,13 @@ class FormContactFindController {
             Number(limit),
             String(search),
             String(orderBy),
-            orderDirection as Prisma.SortOrder // Passando a direção de ordenação aqui
+            orderDirection as Prisma.SortOrder,
+            startDate ? String(startDate) : undefined,
+            endDate ? String(endDate) : undefined
         );
 
         return res.json(contacts);
     }
-
 }
 
 export { FormContactFindController };
--- src/services/form_contact/FormContactFindService.ts
+++ src/services/form_contact/FormContactFindService.ts
@@ -1,5 +1,5 @@
 import prismaClient from "../../prisma";
-import { Prisma } from "@prisma/client"; // Import do Prisma para usar QueryMode
+import { Prisma } from "@prisma/client";
 
 class FormContactFindService {
     async execute(
@@ -7,18 +7,31 @@ class FormContactFindService {
         limit: number = 5,
         search: string = "",
         orderBy: string = "created_at",
-        orderDirection: Prisma.SortOrder = "desc"
+        orderDirection: Prisma.SortOrder = "desc",
+        startDate?: string,
+        endDate?: string
     ) {
         const skip = (page - 1) * limit;
 
-        const whereClause: Prisma.Form_contactWhereInput = search
-            ? {
-                OR: [
-                    { name_user: { contains: search, mode: Prisma.QueryMode.insensitive } },
-                    { email_user: { contains: search, mode: Prisma.QueryMode.insensitive } },
-                ],
-            }
-            : {};
+        // Construção da cláusula 'where' com filtro de texto e data
+        const whereClause: Prisma.Form_contactWhereInput = {
+            ...(
+                search ? {
+                    OR: [
+                        { name_user: { contains: search, mode: Prisma.QueryMode.insensitive } },
+                        { email_user: { contains: search, mode: Prisma.QueryMode.insensitive } },
+                    ]
+                } : {}
+            ),
+            ...(
+                startDate && endDate ? {
+                    created_at: {
+                        gte: new Date(startDate),
+                        lte: new Date(endDate)
+                    }
+                } : {}
+            )
+        };
 
         const all_contacts_form = await prismaClient.form_contact.findMany({
             where: whereClause,
